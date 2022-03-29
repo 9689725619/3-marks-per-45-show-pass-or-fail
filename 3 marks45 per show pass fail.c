5c4876a0d9c8bdd108bd3b051b8298a90cf6bf10
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-29 16:15:25 +0530

    Add files via upload

---
+++ 3 marks45 per show pass fail.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int p1,p2,p3;
+	printf("Enter 3 sun marks :");
+	scanf("%d%d%d",&p1,&p2,&p3);
+	if(p1>45 && p2>45 && p3>45);
+	{
+		printf("pass");
+	}
+		else
+		{
+			printf("fail");
+		}
+	return 0;
+}
